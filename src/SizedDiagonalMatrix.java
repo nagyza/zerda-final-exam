64d81ec5f79479602da4f16326da3cd42b0c4a07
Zsolt Nagy <nagyza@gmail.com>
2017-07-10 09:39:41 +0200

    Create method to prepare a row of the matrix

--- src/SizedDiagonalMatrix.java
+++ src/SizedDiagonalMatrix.java
@@ -7,6 +7,21 @@ public class SizedDiagonalMatrix {
 
   public static int[][] createMatrix(int size) {
     int[][] matrix = new int[size][size];
+    for (int i = 0; i < size; i++) {
+
+    }
     return matrix;
+  }
+
+  public static int[] createRow(int size, int placeOfdiagonal) {
+    int[] row = new int[size];
+    for (int i = 0; i < size; i++) {
+      if (i + 1 == placeOfdiagonal) {
+        row[i] = 1;
+      } else {
+        row[i] = 0;
+      }
+    }
+    return row;
   }
 }
