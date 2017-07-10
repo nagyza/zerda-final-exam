5bd8441d6194743d006016e12821be8f9fb089b7
Zsolt Nagy <nagyza@gmail.com>
2017-07-10 09:51:25 +0200

    Print out matrix in main method

--- src/SizedDiagonalMatrix.java
+++ src/SizedDiagonalMatrix.java
@@ -2,7 +2,17 @@
 public class SizedDiagonalMatrix {
 
   public static void main(String[] args) {
-
+    int size = 5;
+    int[][] matrix = createMatrix(size);
+    for (int i = 0; i < size; i++) {
+      for (int j = 0; j < size; j++) {
+        if (j + 1 < size) {
+          System.out.print(matrix[i][j]);
+        } else {
+          System.out.println(matrix[i][j]);
+        }
+      }
+    }
   }
 
   public static int[][] createMatrix(int size) {
