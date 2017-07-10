1f8e290df7048f4237ee6a4cdbd1d007c459db43
Zsolt Nagy <nagyza@gmail.com>
2017-07-10 09:33:17 +0200

    Prepare class

---
+++ src/SizedDiagonalMatrix.java
@@ -0,0 +1,12 @@
+
+public class SizedDiagonalMatrix {
+
+  public static void main(String[] args) {
+
+  }
+
+  public static int[][] createMatrix(int size) {
+    int[][] matrix = new int[size][size];
+    return matrix;
+  }
+}
